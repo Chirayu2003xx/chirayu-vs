872f477941c6747bb366a298258694d17ee71654
Chirayu2003xx <110988332+Chirayu2003xx@users.noreply.github.com>
2023-10-27 15:51:41 +0530

    hello

--- vsgit.py
+++ vsgit.py
@@ -1,18 +1,23 @@
 def prit(n, isPrime):
-isPrime[0] = isPrime[1] = False
-for i in range(2,n): isPrime[i] = True
-for p in range(2,n+1):
-if (p*p<=n and isPrime[p] == True):
-for i in range(p*2,n+1,p): isPrime[i] = False
-p+=1
+    isPrime[0] = isPrime[1] = False
+    for i in range(2, n + 1):
+        isPrime[i] = True
+    for p in range(2, int(n**0.5) + 1):
+        if isPrime[p]:
+            for i in range(p * p, n + 1, p):
+                isPrime[i] = False
+
 def superPrimes(n):
-isPrime = [1 for i in range(n+1)]
-prit(n, isPrime)
-primes = [0 for i in range(2,n+1)] j=0
-for p in range(2,n+1):
-if(isPrime[p]): primes[j] = p j+=1
-for k in range(j): if(isPrime[k+1]):
-print (primes[k],end=" ")
-n=241
-print ("\nSuper-Primes less than or equal to ", n, " are :",) 
+    isPrime = [1 for i in range(n + 1)]
+    prit(n, isPrime)
+    primes = []
+    for p in range(2, n + 1):
+        if isPrime[p]:
+            primes.append(p)
+    for k in range(len(primes)):
+        if isPrime[primes[k]]:
+            print(primes[k], end=" ")
+
+n = 241
+print("Super-Primes less than or equal to", n, "are:")
 superPrimes(n)
